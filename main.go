99ca76af763c746e0c7f771f8b2ec266f0a2bb3e
snowmerak <go_dev@kakao.com>
2020-12-14 18:30:24 +0900

    create and update for slice

---
+++ main.go
@@ -0,0 +1,123 @@
+package mink
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/snowmerak/mink/gtype"
+)
+
+/*
+Element ... set of essential method for Mink.
+Compare method is comparing I and parameter.
+Filter method is
+*/
+type Element interface{}
+
+//Mink ... struct declared linq method with Element slice.
+type Mink struct {
+	str   string
+	slice []Element
+	maps  map[Element]Element
+	err   error
+	typ   uint8
+}
+
+//From ... from slice or string to Mink
+func From(t interface{}) Mink {
+	g := Mink{str: "", slice: nil, maps: nil, err: nil, typ: 0}
+	typ := reflect.TypeOf(t).Kind()
+	if !(typ == reflect.Slice || typ == reflect.String || typ == reflect.Map) {
+		g.err = fmt.Errorf("%v: From: parameter is invaild type", time.Now())
+		return g
+	}
+	switch typ {
+	case reflect.String:
+		v, ok := t.(string)
+		if !ok {
+			g.err = fmt.Errorf("%v: From: parameter is not vaild string", time.Now())
+		}
+		g.typ = gtype.Str
+		g.str = v
+	case reflect.Slice:
+		v, ok := t.([]Element)
+		if !ok {
+			g.err = fmt.Errorf("%v: From: parameter is not vaild slice", time.Now())
+		}
+		g.typ = gtype.Slice
+		g.slice = v
+	case reflect.Map:
+		v, ok := t.(map[Element]Element)
+		if !ok {
+			g.err = fmt.Errorf("%v: From: parameter is not vaild map", time.Now())
+		}
+		g.typ = gtype.Maps
+		g.maps = v
+
+	}
+	return g
+}
+
+//Filter ... apply criterion
+func (g Mink) Filter(c func(e Element) bool) Mink {
+	switch g.typ {
+	case gtype.Slice:
+		ns := make([]Element, 0, len(g.slice)/2)
+		for _, v := range g.slice {
+			if c(v) {
+				ns = append(ns, v)
+			}
+		}
+		g.slice = ns
+	case gtype.Str:
+		sb := strings.Builder{}
+		for _, v := range g.str {
+			if c(v) {
+				sb.WriteRune(v)
+			}
+		}
+		g.str = sb.String()
+	case gtype.Maps:
+		nm := map[Element]Element{}
+		for k, v := range g.maps {
+			if c(v) {
+				nm[k] = v
+			}
+		}
+		g.maps = nm
+	}
+	return g
+}
+
+/*
+OrderBy ... order by parameter
+the paremeter return when choose a true, not false
+*/
+func (g Mink) OrderBy(f func(a Element, b Element) bool) Mink {
+	if g.typ != gtype.Slice {
+		return g
+	}
+	rt := <-asyncMergeSortSlice(g.slice, f)
+	g.slice = rt
+	return g
+}
+
+/*
+Unwrap ... unwrap to interface{} can convert to slice, map, string, error
+*/
+func (g Mink) Unwrap() interface{} {
+	if g.err != nil {
+		return g.err
+	}
+	switch g.typ {
+	case gtype.Str:
+		return g.str
+	case gtype.Slice:
+		return g.slice
+	case gtype.Maps:
+		return g.maps
+	}
+	return nil
+}
